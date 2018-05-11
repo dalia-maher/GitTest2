88aa00d81ecebd553182cc566a3edce10a77d8fd
AbdAllhkandil <38462396+AbdAllhkandil@users.noreply.github.com>
2018-05-11 15:56:08 +0200

    Adding new project

---
+++ oop_telusko/src/test/Motorela.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Motorela extends Samsung {
+
+	public void dance() {
+		System.out.println("dance");
+	}
+}
---
+++ oop_telusko/src/test/Samsung.java
@@ -0,0 +1,14 @@
+package test;
+
+ public class Samsung  {
+	 String s;
+    public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+
+    
+   
+}
